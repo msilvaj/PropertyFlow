5e6309ecbdab546ce6db4fa23680540565a41507
Miqueias Silva <miqueias.ufpi@gmail.com>
2026-01-30 05:48:48 -0300

    auth: final configuration for JSON support in Devise and ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  respond_to :html, :json
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: -> { request.format.json? }
   before_action :set_locale
